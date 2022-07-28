d0d580c7dca52f10a7008b177984a706c1063741
Maniusis Justinas (Student Com21) <Justinas.Maniusis@stud.inf-unibz.it>
2022-07-28 16:11:10 +0300

    Meeting object added

---
+++ Category.cs
@@ -0,0 +1,6 @@
+public enum Category {
+    CodeMonkey,
+    Hub,
+    Short,
+    TeamBuilding
+}
---
+++ Meeting.cs
@@ -0,0 +1,13 @@
+public class Meeting {
+    // meeting data is store in a JSON
+
+    private String name;
+
+    private String responsiblePerson;
+
+    private String description;
+    private Category category;
+    private Type type;
+    private DateTime StartDate;
+    private DateTime EndDate;
+}
